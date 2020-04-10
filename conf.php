44956fefcb8723297485e331b1feee2f5add470e
glu000 <gl@gl.co.at>
2020-04-11 00:18:34 +0200

    update

--- conf.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$bbb_secret="98b204dc3310d8df98e1bfe986fd61bc";
-$servername="https://bbb.gl.co.at";
-
-$filename = "/var/log/bbb-usage.csv";
-$timezone = "Europe/Vienna";
-$delimiter = ";";
