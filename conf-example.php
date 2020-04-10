cd50db989f4c41e30dc1b09e53c4379b41a1ab1f
glu000 <gl@gl.co.at>
2020-04-11 00:41:17 +0200

    Create conf-example.php

---
+++ conf-example.php
@@ -0,0 +1,10 @@
+<?php
+
+// Enter your settings and save as conf.php
+
+$bbb_secret="your BBB secret";
+$servername="your server name";         // e.g. https://bbb.yourdomain.com
+
+$filename = "/var/log/bbb-usage.csv";   // filename for data file
+$timezone = "Europe/Vienna";            // your timezone
+$delimiter = ";";                       // delimiter for data file
