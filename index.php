4fb63d9f0f2bfe95ea02357881e56ef0836a0250
glu000 <gl@gl.co.at>
2020-04-16 00:50:27 +0200

    Update index.php

--- index.php
+++ index.php
@@ -59,11 +59,11 @@ if (($handle = fopen($filename, "r")) !== FALSE) {
             $srvname = $data [$serveridx];
             if (!in_array($srvname, $server_arr)) $server_arr [] = $srvname;
 
-            $ydata['meetingCount'][$srvname][$row] = (int)$data[4];
-            $ydata['participantCount'][$srvname][$row] = (int)$data[5];
-            $ydata['voiceParticipantCount'][$srvname][$row] = (int)$data[6];
-            $ydata['videoCount'][$srvname][$row] = (int)$data[7];
-            $ydata['breakoutCount'][$srvname][$row] = (int)$data[8];
+            $ydata['meetingCount'][$srvname][$row] = (int)$data[4+(6*$i)];
+            $ydata['participantCount'][$srvname][$row] = (int)$data[5+(6*$i)];
+            $ydata['voiceParticipantCount'][$srvname][$row] = (int)$data[6+(6*$i)];
+            $ydata['videoCount'][$srvname][$row] = (int)$data[7+(6*$i)];
+            $ydata['breakoutCount'][$srvname][$row] = (int)$data[8+(6*$i)];
 
             $serveridx += 6;
         }
@@ -79,6 +79,7 @@ else
 }
 
 $allservers = $server_arr;
+//$allservers[] = "Sum";
 if (!empty ($selserver))
 {
     foreach ($server_arr as $key => $server)
@@ -100,12 +101,19 @@ for ($i=0; $i<$row; $i++)
     {
         foreach ($ydata as $key => $stat) {
                 $gdata [$key][$i][0] = 'new Date(' . ($xdata[$i] * 1000) . ')';
+                $sum = 0;
                 foreach ($server_arr as $key1 => $srvname) {
                     if (!isset ($stat[$srvname][$i])) $ydata[$key][$srvname][$i] = 0;
                     $gdata [$key][$i][] = $ydata[$key][$srvname][$i];
+                    $sum += $ydata[$key][$srvname][$i];
                     // Tooltip
                     $gdata [$key][$i][] = "'" . date('y-m-d H:i', $xdata[$i]) . " - " . $srvname . ": " . $ydata[$key][$srvname][$i] . "'";
                 }
+                // Sum
+                //$gdata [$key][$i][] = $sum;
+                //$gdata [$key][$i][] = "'" . date('y-m-d H:i', $xdata[$i]) . " - Sum: " . $sum . "'";
+
+
         }
     }
 }
@@ -145,6 +153,8 @@ if (!empty($gdata))
             $script .= "data.addColumn('number', '".$srvname."'); \n";
             $script .= "data.addColumn({type: 'string', role: 'tooltip'}); \n";
         }
+        //$script .= "data.addColumn('number', 'Sum'); \n";
+        //$script .= "data.addColumn({type: 'string', role: 'tooltip'}); \n";
 
         $stat = array_values($stat);
         $js_gdata = json_encode($stat);
@@ -154,13 +164,13 @@ if (!empty($gdata))
 
         //'#CB4335', '#2471A3', '#138D75', '#D4AC0D', '#2E4053'        colors: ['#10a513', '#097138'],
 
-        $script .= "var options = { height:400, colors: ['#CB4335', '#2471A3', '#D4AC0D', '#138D75', '#2E4053'], \n";
+        $script .= "var options = { height:400, colors: ['#CB4335', '#2471A3', '#D4AC0D', '#138D75', '#2E4053'], isStacked: 'true', \n";
         $script .= "hAxis: { title: 'Date', format: 'yy-MM-dd HH:mm' }, \n";
         $script .= "title: '".$title[$key]."', \n";
         //$script .= "vAxis: { title: '".$title[$key]."' }, \n";
         $script .= "}; \n";
 
-        $script .= "var chart = new google.visualization.LineChart(document.getElementById('chart_".$key."')); \n";
+        $script .= "var chart = new google.visualization.AreaChart(document.getElementById('chart_".$key."')); \n";
         $script .= "chart.draw(data, options); \n";
 
         //$script .= "var chart = new google.charts.Bar(document.getElementById('chart_".$key."')); ";
