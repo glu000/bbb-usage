689e4963427809a2997821ccddc99d712a16952a
glu000 <gl@gl.co.at>
2020-05-12 01:18:18 +0200

    Update conf-example.php

--- conf-example.php
+++ conf-example.php
@@ -4,20 +4,18 @@
 
 
 // You can define different secret to filter bbb-origin-server-name
-// Use * as wildcard to show all origin servers
-$secrets = array ("<Secret_to_show_all_origin_servers>" => "*",
+// Use % as wildcard to show all origin servers
+$secrets = array ("<Secret_to_show_all_origin_servers>" => "%",
                   "<SecondSecret>" => "origin.server1.tld",
                   "<ThirdSecret>"  => "origin.server2.tld" );
 
 $bbb_secret="your BBB secret";
 $servername="your server name";         // e.g. https://bbb.yourdomain.com
 
-$filename = "/var/log/bbb-usage.csv";   // filename for data file
 $timezone = "Europe/Vienna";            // your timezone
-$delimiter = ";";                       // delimiter for data file
 
 $db_server = "localhost";
-$db_port = "12987";
-$db_user = "bbb";
-$db_password = "BBB-Usage!";
-$db_name = "bbb-usage";
+$db_port = "3306";
+$db_user = "<username>";
+$db_password = "<password>";
+$db_name = "bbb-usage";                 // database has to be created before starting bbb-usage
