42c9338ef1924d1668e6227002fec9c1bfb1beed
glu000 <gl@gl.co.at>
2020-05-11 11:43:05 +0200

    update

--- collect.php
+++ collect.php
@@ -12,25 +12,84 @@ $data = getCurrentData();
 $day = date('Y-m-d');
 $time = date('H:i');
 
-$output  = $day . $delimiter;
-$output .= $time . $delimiter;
-$output .= count ($data);
+$exec_loads = sys_getloadavg();
+$exec_cores = trim(shell_exec("grep -P '^processor' /proc/cpuinfo|wc -l"));
+$cpu = round($exec_loads[1]/($exec_cores + 1)*100, 0);
 
-foreach ($data as $server => $stats)
+$exec_free = explode("\n", trim(shell_exec('free')));
+$get_mem = preg_split("/[\s]+/", $exec_free[1]);
+$mem = round($get_mem[2]/$get_mem[1]*100, 0);
+
+if ($db_name != "")
 {
-    $output .= $delimiter;
+    // put data into database
 
-    $output .= $server . $delimiter;
-    $output .= $stats['meetingCount'] . $delimiter;
-    $output .= $stats['participantCount'] . $delimiter;
-    $output .= $stats['voiceParticipantCount'] . $delimiter;
-    $output .= $stats['videoCount'] . $delimiter;
-    $output .= $stats['breakoutCount'];
+    $conn = new mysqli($db_server, $db_user, $db_password, $db_name);
 
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "CREATE TABLE IF NOT EXISTS bbb_usage_data (
+                    id INT(9) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                    ts TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                    server VARCHAR(255),
+                    meeting_count SMALLINT UNSIGNED,
+                    participant_count SMALLINT UNSIGNED,
+                    voice_participant_count SMALLINT UNSIGNED,                                        
+                    video_count SMALLINT UNSIGNED,
+                    breakout_count SMALLINT UNSIGNED,
+                    cpu SMALLINT UNSIGNED,
+                    mem SMALLINT UNSIGNED,
+                    netout INT (11) UNSIGNED,
+                    netin INT (11) UNSIGNED,
+                    INDEX ts_index (ts)                                       
+                    )";
+
+    $ressql = $conn->query($sql);
+
+    foreach ($data as $server => $stats)
+    {
+        $mc = $stats['meetingCount'];
+        $pc = $stats['participantCount'];
+        $vpc = $stats['voiceParticipantCount'];
+        $vc = $stats['videoCount'];
+        $bc = $stats['breakoutCount'];
+
+        $sql = "INSERT INTO bbb_usage_data (server, meeting_count, participant_count, voice_participant_count, video_count, breakout_count, cpu, mem)
+                VALUES ('$server', $mc, $pc, $vpc, $vc, $bc, $cpu, $mem)";
+
+        $ressql = $conn->query($sql);
+    }
+
+
+    $conn->close();
+}
+else
+{
+    // put data into csv-file
+
+    $output  = $day . $delimiter;
+    $output .= $time . $delimiter;
+    $output .= count ($data);
+
+    foreach ($data as $server => $stats)
+    {
+        $output .= $delimiter;
+
+        $output .= $server . $delimiter;
+        $output .= $stats['meetingCount'] . $delimiter;
+        $output .= $stats['participantCount'] . $delimiter;
+        $output .= $stats['voiceParticipantCount'] . $delimiter;
+        $output .= $stats['videoCount'] . $delimiter;
+        $output .= $stats['breakoutCount'];
+
+    }
+
+    $output .= "\n";
+
+    print $output;
+
+    file_put_contents($filename, $output, FILE_APPEND);
 }
 
-$output .= "\n";
-
-print $output;
-
-file_put_contents($filename, $output, FILE_APPEND);
--- conf-example.php
+++ conf-example.php
@@ -2,9 +2,22 @@
 
 // Enter your settings and save as conf.php
 
+
+// You can define different secret to filter bbb-origin-server-name
+// Use * as wildcard to show all origin servers
+$secrets = array ("<Secret_to_show_all_origin_servers>" => "*",
+                  "<SecondSecret>" => "origin.server1.tld",
+                  "<ThirdSecret>"  => "origin.server2.tld" );
+
 $bbb_secret="your BBB secret";
 $servername="your server name";         // e.g. https://bbb.yourdomain.com
 
 $filename = "/var/log/bbb-usage.csv";   // filename for data file
 $timezone = "Europe/Vienna";            // your timezone
 $delimiter = ";";                       // delimiter for data file
+
+$db_server = "localhost";
+$db_port = "12987";
+$db_user = "bbb";
+$db_password = "BBB-Usage!";
+$db_name = "bbb-usage";
