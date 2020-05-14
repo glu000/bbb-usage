b6a7e404674b45644785ba9fb45ebe4af47bec5a
glu000 <gl@gl.co.at>
2020-05-14 23:22:50 +0200

    Update conf-example.php

--- conf-example.php
+++ conf-example.php
@@ -3,11 +3,17 @@
 // Enter your settings and save as conf.php
 
 
-// You can define different secret to filter bbb-origin-server-name
+// You can define different secrets (access codes) to filter different frontends (API-parameter bbb-origin-server-name)
 // Use % as wildcard to show all origin servers
+// Don't mix up these secrets with your BBB-secret
 $secrets = array ("<Secret_to_show_all_origin_servers>" => "%",
                   "<SecondSecret>" => "origin.server1.tld",
                   "<ThirdSecret>"  => "origin.server2.tld" );
+
+// If you just have one frontend (e.g. Greenlight) you can use following seeting
+$secrets = array ("MySecret" => "%");
+//
+// When opening the page enter "MySecret" and hit return
 
 $bbb_secret="your BBB secret";
 $servername="your server name";         // e.g. https://bbb.yourdomain.com
